25e9da494787e6db816b42389aec1370b34b62ba
kieuanhdev <kieuanh.dev@gmail.com>
2025-09-05 10:05:44 +0700

    use lombok

---
+++ src/main/java/dev/kieuanh/SpringBootFullSkill/dto/request/SampleDTO.java
@@ -0,0 +1,21 @@
+package dev.kieuanh.SpringBootFullSkill.dto.request;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//@EqualsAndHashCode
+
+
+//DE NHU TREN LA SAI LAM
+
+
+public class SampleDTO implements Serializable {
+    private Integer id;
+    private String name;
+}
--- src/main/java/dev/kieuanh/SpringBootFullSkill/dto/request/UserRequestDTO.java
+++ src/main/java/dev/kieuanh/SpringBootFullSkill/dto/request/UserRequestDTO.java
@@ -3,6 +3,7 @@ package dev.kieuanh.SpringBootFullSkill.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.kieuanh.SpringBootFullSkill.util.*;
 import jakarta.validation.constraints.*;
+import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static dev.kieuanh.SpringBootFullSkill.util.Gender.*;
 
+@Getter
 public class UserRequestDTO implements Serializable {
     @NotBlank(message = "firstName không được null, không được rỗng, và không chỉ chứa khoảng trắng.")
     private String firstName;
@@ -41,92 +43,5 @@ public class UserRequestDTO implements Serializable {
     @EnumValue(name = "type", enumClass = UserType.class)
     private String type;
 
-    public String getType() {
-        return type;
-    }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public UserRequestDTO() {
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public UserRequestDTO(String firstName, String lastName, String email, String phone) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public UserRequestDTO(String firstName, String lastName, String email, String phone, Date birthDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.birthDate = birthDate;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public List<String> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(List<String> permissions) {
-        this.permissions = permissions;
-    }
-
-    public UserStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(UserStatus status) {
-        this.status = status;
-    }
 }
